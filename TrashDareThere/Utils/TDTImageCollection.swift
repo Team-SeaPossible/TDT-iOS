11ed7e035603094113d2335fa9dd6a8303a147f8
BoMin <lbmleebm@gmail.com>
2024-02-08 18:22:47 +0900

    FEAT: Add ImageCollection

---
+++ TrashDareThere/Utils/TDTImageCollection.swift
@@ -0,0 +1,17 @@
+//
+//  TDTImageCollections.swift
+//  TrashDareThere
+//
+//  Created by BoMin Lee on 2/8/24.
+//
+
+import UIKit
+
+final class TDTImageCollection {
+    private init() { }
+    
+    static let leftArrowImage: UIImage = #imageLiteral(resourceName: "arrow_left")
+    static let rightArrowImage: UIImage = #imageLiteral(resourceName: "arrow_right")
+    static let fish: UIImage = #imageLiteral(resourceName: "fish")
+    
+}
